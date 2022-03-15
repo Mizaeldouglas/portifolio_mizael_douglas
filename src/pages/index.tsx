22b551e8f4bdb254b0a603b8a9ea17bef70e2546
Mizaeldouglas <mizaeldouglas2@gmail.com>
2022-03-14 21:12:07 -0300

    Terminado pagina Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import styles from '../styles/home.module.scss'
 
+import Image from 'next/image'
+import TechsImage from '../../public/images/techs.svg'
+
 
 export default function Home() {
   return (
@@ -23,6 +26,43 @@ export default function Home() {
 					<img src='/images/banner-conteudos.png' alt="conteudos" />
 
 			  </div>
+			  
+			  <hr className={styles.divisor}/>
+
+			<div className={styles.sectionContainer} >
+				<section>
+					<h2>Aprenda criar aplicativos para Android e iOS</h2>
+         			 <span>Você vai descobrir o jeito mais moderno de desenvolver apps nativos para iOS e Android, construindo aplicativos do zero até aplicativos.</span>
+				</section>
+				<img src="/images/financasApp.png" alt="app" />
+
+			</div>
+
+			<hr className={styles.divisor}/>
+
+			<div className={styles.sectionContainer} >
+				<img src="/images/webDev.png" alt="app" />
+
+				<section>
+					<h2>Aprenda criar sistemas web</h2>
+         			<span>Criar sistemas web, sites usando as tecnologias mais modernas e requisitadas pelo mercado.</span>
+				</section>
+
+			</div>
+
+		<div className={styles.nextLevelContent}>
+
+			<Image src={TechsImage} alt='tecnologias'/>
+			<h2>Mais de <span className={styles.alunos}>15 mil</span> já levaram sua carreira ao próximo nivel.</h2>
+        	<span>E você vai perder a chance de evoluir de uma vez por todas?</span>
+			<a>
+         		<button>ACESSAR TURMA!</button>
+        	</a>
+
+		</div>
+
+
+
 		  </main>
 	  </>
     </div>
