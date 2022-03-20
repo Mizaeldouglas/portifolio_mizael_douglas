6b203b5eee188970e204413afaf6b7329b36ec78
Mizaeldouglas <mizaeldouglas2@gmail.com>
2022-03-19 22:13:29 -0300

    colocando prismic na pagina de blog

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,8 +1,9 @@
 import styles from './stytes.module.scss'
+
+import { useState,useEffect } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import tumbImg from '../../../public/images/thumb.png'
 
 import { FiChevronLeft,FiChevronsLeft,FiChevronRight,FiChevronsRight } from 'react-icons/fi'
 
@@ -15,8 +16,26 @@ import { RichText } from 'prismic-dom'
 
 
 
+type Post = {
+			slug:string,
+			title:string,
+			descripton: string,
+			cover:string,
+			updateAt:string,
+}
 
-export default function Posts(){
+interface PostsProps{
+	post:Post[];
+}
+
+
+
+export default function Posts({ post:PostsBlog }: PostsProps){
+
+	const [post, setPost] = useState(PostsBlog || []);
+
+
+	// console.log(post)
 	return(
 		<>
 			<Head>
@@ -24,20 +43,24 @@ export default function Posts(){
 			</Head>
 			<main className={styles.container}>
 				<div className={styles.posts}>
-					<Link href='/'>
-						<a>
+					{post.map( post => (
+						<Link key={post.slug} href={`/posts/${post.slug}`}>
+						<a key={post.slug}>
 							<Image 
-								src={tumbImg} 
-								alt='tituloDoPost'
+								src={post.cover} 
+								alt={post.title}
 								width={720}
 								height={410}
 								quality={100}
+								placeholder='blur'
+								blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+PBvHgAFdAKOz8a+mQAAAABJRU5ErkJggg=='
 							/>
-							<strong>Criando meu primeiro aplicativo</strong>
-							<time>14 Julho 2021</time>
-							<p>Hoje vamos criar o controle de mostrar a senha no input, uma opção para os nossos formulários de cadastro e login. Mas chega de conversa e bora pro código junto comigo que o vídeo está show de bola!</p>
+							<strong>{post.title}</strong>
+							<time>{post.updateAt}</time>
+							<p>{post.descripton}</p>
 						</a>
 					</Link>
+					))}
 
 					<div className={styles.buttonNavigate}>
 						<div>
@@ -71,14 +94,35 @@ export const getStaticProps: GetStaticProps = async () => {
 	const prismic = getPrismicClient()
 
 	const response =  await prismic.query([
-		Prismic.Predicates.at('document.type','post')
-	])
+		Prismic.Predicates.at('document.type','UID')
+	],{
+		orderings: '[document.last_publication_date desc]', //Ordenar pelo mais recente
+		fetch: ['UID.title','UID.descripton','UID.cover'],
+		pageSize: 3
+	})
+	// console.log(JSON.stringify(response,null,2))
+
+	const post = response.results.map( post => {
+		return{
+			slug:post.uid,
+			title:RichText.asText(post.data.title),
+			descripton: post.data.descripton.find(content => content.type === 'paragraph')?.text ?? '',
+			cover:post.data.cover.url,
+			updateAt:new Date(post.last_publication_date).toLocaleDateString('pt-BR',{
+				day: '2-digit',
+				month: 'long',
+				year: 'numeric'
+			})
+
+		}
+	} )
 
 
 	return{
 		props:{
-
-		}
+			post
+		},
+		revalidate: 60 * 30 //atualiza a cada 30 minutos
 	}
 	
 
